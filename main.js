6ba03371ef4c32fcc6c6ae8d1ac9d51d37064d87
Amit-codex <amit.algoartisans@gmail.com>
2024-02-24 21:42:24 -1200

    add function for inputs like amount, description and category

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Expense Data store
 let expense = [];  
 
-// Dom Element to manipulate the form 
+// DOM Element to manipulate the form 
 const expenseForm = document.getElementById('expense-form');
 const amountInput = document.getElementById('amount');
 const descriptionInput = document.getElementById('description');
@@ -9,3 +9,15 @@ const categoryInput = document.getElementById('category');
 const expenseList = document.getElementById('expense-list');
 const totalExpense = document.getElementById('total-expenses');
 const clearButton = document.getElementById('clear');
+
+//Function
+function addExpense(amount, description,category){
+    const expense = {
+        amount: parseFloat(amount),
+        description,category
+    };
+
+    expense.push(expense);
+    updateUI();
+    saveToLocalStorage();
+}
