3e3e1f7ec2d1bb2e19b00ccb9b4cd499c8ef5255
Amit-codex <amit.algoartisans@gmail.com>
2024-02-24 21:34:02 -1200

    write the code for js and add data store code, DOM code

---
+++ main.js
@@ -0,0 +1,11 @@
+// Expense Data store
+let expense = [];  
+
+// Dom Element to manipulate the form 
+const expenseForm = document.getElementById('expense-form');
+const amountInput = document.getElementById('amount');
+const descriptionInput = document.getElementById('description');
+const categoryInput = document.getElementById('category');
+const expenseList = document.getElementById('expense-list');
+const totalExpense = document.getElementById('total-expenses');
+const clearButton = document.getElementById('clear');
